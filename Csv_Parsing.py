f511e137d7b3a039e01085af0c828fb5069196a4
yuxia7 <yu.xia.2000@hotmail.com>
2020-02-23 13:02:12 +0000

    final push

--- Csv_Parsing.py
+++ Csv_Parsing.py
@@ -3,39 +3,77 @@
 """
 import csv
 import datetime
+import time
+import matplotlib.pyplot as mpl
 
 class Csv_Parsing :
-	"""docstring for ClassName"""
-	def __init__(self):
-		pass
+        data_set = []
+        def __init__(self):
+                pass
+        
+        def listAdd(self,XVal, YVal, DatasetTuple, XList, YList):
+                XPos =0
+                YPos =3
+                VarList = DatasetTuple[0]
+                DataBlck = DatasetTuple[1]
+                for i in range(3):
+                        if XVal == VarList[i]:
+                                XPos = i
+                for i in range(len(VarList)):
+                        if YVal == VarList[i]:  
+                                YPos = i
+                for i in range(len(DataBlck)):
+                        if not(DataBlck[i][XPos] in XList):
+                                XList.append(DataBlck[i][XPos])
+                                YList.append(DataBlck[i][YPos])
+                mpl.plot(XList, YList)
+                mpl.ylabel(YVal)
+                mpl.xlabel(XVal)
+                mpl.show()
+                return XList,YList
+        
+        def read_csv_file(self,file):
+                Listt=([0],[0])
+                with open(file) as csv_file:		
+                        csv_reader = csv.reader(csv_file, delimiter=',')
+                        label_names = []
+                        data_set = []
+                        line_count = 0
+                        for row in csv_reader:
+                                if line_count == 0 :
+                                        #creates a list of all the y-value names 
+                                        label_names = row
+                                        print(row)
+                                        line_count += 1
+                        else :	
+                                data_set = row
+                                print(row)
+                                line_count += 1
+                        
+                        Listt=self.listAdd('Date','Barometric Pressure',(label_names, data_set),Listt[0],Listt[1])
+                        #return label_names, data_set
+        """
+        def read_csv_file(self,file) :
+                data_set = []
+                with open(file) as csv_file:
+                        csv_reader = csv.reader(csv_file, delimiter=',')
+                        label_names = []
+                        line_count = 0
+                        for row in csv_reader:
 
+                                if line_count == 0 :
+                                        #creates a list of all the y-value names 
+                                        label_names = row
+                                        line_count += 1
+                                if line_count != 0 :
+                                        data_set.append(row)
+                                        line_count += 1
+                return label_names, data_set
+        """
 
-	"""
-		Function writes all the data points into a 
-		Parameters:
-			file - csv file being written into by the function
-			data - a dictionary of the form {x_data_point : y_data_points}
-			where x data set is string of date-time and y data set is the barametric pressure
-			at a certain position in a string of lat and long
-		Returns:
-			returns a list of the order of variables and a 2d list of the data set 
-	"""
-	def read_csv_file(self,file) :
-		data_set = []
-		with open(file) as csv_file:
-			csv_reader = csv.reader(csv_file, delimiter=',')
-			label_names = []
-			line_count = 0
-			for row in csv_reader:
-
-				if line_count == 0 :
-    					#creates a list of all the y-value names 
-					label_names = row
-					line_count += 1
-				if line_count != 0 :
-					data_set.append(row)
-					line_count += 1
-		return label_names, data_set
-
-
-
+if __name__ == "__main__":
+        while 1:
+                file = "data.csv"
+                csv_penis = Csv_Parsing()
+                csv_penis.read_csv_file(file)
+                time.sleep(10000)
--- plotFunctions.py
+++ plotFunctions.py
@@ -7,8 +7,7 @@ import matplotlib.pyplot as mpl
 """
 
 
-def listAdd(XVal, YVal, DatasetTuple, ExistingData):
-    XList=[]
+def listAdd(XVal, YVal, DatasetTuple, XList, YList):
     XPos =0
     YPos =3
     VarList = DatasetTuple[0]
@@ -17,33 +16,36 @@ def listAdd(XVal, YVal, DatasetTuple, ExistingData):
         if XVal == VarList[i]:
             XPos = i
     for i in range(len(VarList)):
-        print (len(VarList))
         if YVal == VarList[i]:  
             YPos = i
     for i in range(len(DataBlck)):
-        print(i)
-        print( DataBlck[i][XPos])
+        
         if not(DataBlck[i][XPos] in XList):
             XList.append(DataBlck[i][XPos])
-    for i in range(len(DataBlck)):
-        print(i)
-        print(DataBlck[i][YPos])
-        ExistingData.append(DataBlck[i][YPos])
-   
-    mpl.plot(XList, ExistingData)
+            YList.append(DataBlck[i][YPos])
+       
+    mpl.plot(XList, YList)
     mpl.ylabel(YVal)
     mpl.xlabel(XVal)
     mpl.show()
-    return ExistingData
+    return XList,YList
 
 
 
 if __name__ == "__main__":
-    Data = (['Date', 'Model', 'Location', 'Barometric Pressure', 'Test1'], [['20 21 2020','Smangsmug', '-33,22', '1000', 'TEST A'], ['20 21 2020','Smangsmug', '-33,20', '3000', 'TEST B'], ['20 21 2020', 'ePhone', '-33,18', '2800', 'TEST C'], ['20 21 2020','Smol', '-34,27', '1200', 'TEST D']])
-    ExistingData =[]
+    Data = (['Date', 'Model', 'Location', 'Barometric Pressure', 'Test1'], [['20 21 2020 1PM','Smangsmug', '-33,22', '1000', 'TEST A'], ['20 21 2020 2PM','Smangsmug', '-33,20', '3000', 'TEST B'], ['20 21 2020 3PM', 'ePhone', '-33,18', '2800', 'TEST C'], ['20 21 2020 4PM','Smol', '-34,27', '1200', 'TEST D']])
+    ExistingData=([],[])
     while 1:
-        a = input("Gib X Parameter, plz: ") 
+        a = input("Gib X Parameter, plz: ")
         print("Thank.")
         b = input("Gib Y Parameter, plz: ")  
         print("Thank.")
-        ExistingData = listAdd(a,b,Data, ExistingData)
+        ExistingData = listAdd(a,b,Data, ExistingData[0], ExistingData[1])
+        c = input("Gib X Parameter, plz: ")
+        print("Thank.")
+        d = input("Gib Y Parameter, plz: ")
+        print("Thank.")
+        ExistingData = listAdd(a,b,Data, ExistingData[0], ExistingData[1])
+        if (a != c or b !=d):
+            ExistingData=([],[])
+         
